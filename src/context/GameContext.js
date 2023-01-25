b558e4e77829bc3f375376415d3f1fa96ffb8c26
Ben Greanya <bengreanya@gmail.com>
2023-01-25 14:54:53 -0800

    refactored setSelected

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,11 +1,14 @@
-import { useContext, useState } from 'react';
-import { createContext } from 'react';
+import { useContext, useState, createContext } from 'react';
 
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [selectedCard, setSelectedCard] = useState();
-  return <GameContext.Provider value={{ selectedCard, setSelectedCard }}></GameContext.Provider>;
+  return (
+    <GameContext.Provider value={{ selectedCard, setSelectedCard }}>
+      {children}
+    </GameContext.Provider>
+  );
 };
 
 const useGameContext = () => {
