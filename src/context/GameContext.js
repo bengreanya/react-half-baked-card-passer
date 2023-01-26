3c1f904ba3932a550ec2a5900c9cdfcb47db60cd
Ben Greanya <bengreanya@gmail.com>
2023-01-25 17:29:45 -0800

    refactored to and seto

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -5,8 +5,9 @@ const GameContext = createContext();
 const GameProvider = ({ children }) => {
   const [selectedCard, setSelectedCard] = useState();
   const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
   return (
-    <GameContext.Provider value={{ selectedCard, setSelectedCard, from, setFrom }}>
+    <GameContext.Provider value={{ selectedCard, setSelectedCard, from, setFrom, to, setTo }}>
       {children}
     </GameContext.Provider>
   );
